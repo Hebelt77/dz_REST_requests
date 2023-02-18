0b7a110a6c245725e4a99b2af085a39295dd8464
Hebelt77 <hebelt@mail.ru>
2023-02-18 23:39:47 +0300

    add hh telebot

--- hh_parser.py
+++ hh_parser.py
@@ -1,120 +1,146 @@
 import re
 import json
+import threading
+
 import requests
 import pprint
+import time
 from collections import Counter
 from statistics import mean
-from string import ascii_letters
+from csv import DictWriter
 
 url_vacancies = 'https://api.hh.ru/vacancies'
 
 n = 'Python Developer OR Python Junior OR Junior Python'
 
-name = input('Введите название интересующей вакансии: ')
-num_page = input("Сколько страниц обработать")
 
-def request():  # Функция get запроса по параметрам
-    result = requests.get(url_vacancies, params=params).json()
-    return result
+# name = input('Введите название интересующей вакансии: ')
+# num_page = input("Сколько страниц обработать: ")
+class Parser:
 
+    def __init__(self):
+        self.result = {}
+        self.name_csv = None
+        self.str = None
 
-get = requests.get(url_vacancies).json()
+    def __str__(self):
+        stroka = self.str
+        return f"{stroka}"
 
-num_vacancies = 0   # Количество вакансий
-employment = []  # Список вакансий по занятости
-url_alternate = []  # Список вакансий со стажировками
-area = []  # Список количества вакансий по городам
-skills = []  # Список ключевых требований
+    def output_print(self, stroka='Пусто'):
+        self.str = stroka
+        print(stroka)
+        return stroka
 
-salary = {  # Диапазон зарплат
-    'from': [],
-    'to': []
-}
+    def start_parser(self, name_vacancies, num_page=7):
 
-for page in range(get['pages']):            # Перебираем страницы с вакансиями
-    if page > num_page:
-        break
-    else:
-        print(f'Ищем на странице {page}')
-        params = {                          # Подставляем название вакансии и номер страницы
-            'text': f'NAME:({name})',
-            'page': page
+        get = requests.get(url_vacancies).json()
+
+        num_vacancies = 0  # Количество вакансий
+        employment = []  # Список вакансий по занятости
+        url_alternate = []  # Список вакансий со стажировками
+        area = []  # Список количества вакансий по городам
+        skills = []  # Список ключевых требований
+
+        salary = {  # Диапазон зарплат
+            'from': [],
+            'to': []
         }
-    for item in request()['items']:  # Перебираем вакансии из запроса
-        url = requests.get(item['url']).json()  # Делаем запрос на вакансию
-        num_vacancies += 1  # Считаем вакансии
+        for page in range(1, get['pages']):  # Перебираем страницы с вакансиями
+            if page > int(num_page):
+                break
+            else:
+                self.output_print(f'Ищем на странице {page}')
+                params = {  # Подставляем название вакансии и номер страницы
+                    'text': f'NAME:({name_vacancies})',
+                    'page': page
+                    # 'area': '1446'
+                }
+            for item in requests.get(url_vacancies, params=params).json()['items']:  # Перебираем вакансии из запроса
+                url = requests.get(item['url']).json()  # Делаем запрос на вакансию
+                num_vacancies += 1  # Считаем вакансии
 
-        if url['salary']:                       # Если в вакансии указана зарплата
-            if url['salary']['from']:           # Сортируем в список по диапазону зарплат от и до
-                salary['from'].append(url['salary']['from'])
-            if url['salary']['to']:
-                salary['to'].append(url['salary']['to'])
+                if url['salary']:  # Если в вакансии указана зарплата
+                    if url['salary']['from']:  # Сортируем в список по диапазону зарплат от и до
+                        salary['from'].append(url['salary']['from'])
+                    if url['salary']['to']:
+                        salary['to'].append(url['salary']['to'])
 
-        employment.append(url['employment']['name'])  # Сортируем в список по виду занятости
-        if url['employment']['name'] in 'Стажировка':  # Если есть стажировка, добавляем в список
-            url_alternate.append(url['alternate_url'])
+                employment.append(url['employment']['name'])  # Сортируем в список по виду занятости
+                if url['employment']['name'] in 'Стажировка':  # Если есть стажировка, добавляем в список
+                    url_alternate.append(url['alternate_url'])
 
-        if url['area']:                         # Ищем количество вакансий по городам
-            area.append(url['area']['name'])
+                if url['area']:  # Ищем количество вакансий по городам
+                    area.append(url['area']['name'])
 
-        for skill in url['key_skills']:                 # Поиск по ключевым требованиям
-            skills.append(skill['name'])
-        if item['snippet']['requirement']:
-            for skill in item['snippet']['requirement']:            # Поиск требований по описанию
-                skills_re = re.findall(r'\s[A-Za-z-/]+', skill)     # Сортировка результата по символам
-                skill_lst = list(filter(lambda x: len(x) > 2, skills_re))
-                skills.extend(skill_lst)
+                for skill in url['key_skills']:  # Поиск по ключевым требованиям
+                    skills.append(skill['name'])
+                if item['snippet']['requirement']:
+                    for skill in item['snippet']['requirement']:  # Поиск требований по описанию
+                        skills_re = re.findall(r'\s[A-Za-z-/]+', skill)  # Сортировка результата по символам
+                        skill_lst = list(filter(lambda x: len(x) > 2, skills_re))
+                        skills.extend(skill_lst)
 
-# Формируем список с результатами парсинга
-result = [{
-    'keywords': name,
-    'total_vacancies': num_vacancies,
-    'requirements': [],
-    'salary': {
-        'from': round(mean(salary['from']), 0),
-        'to': round(mean(salary['to']), 0)
-    },
-    'employment': [],
-    'area': []
+        # Формируем список с результатами парсинга
+        self.result = {
+            'keywords': name_vacancies,
+            'total_vacancies': num_vacancies,
+            'requirements': [],
+            'salary': {
+                'from': round(mean(salary['from']), 0) if salary['from'] else None,
+                'to': round(mean(salary['to']), 0) if salary['to'] else None
+            },
+            'employment': [],
+            'area': []
+        }
 
-}]
+        skills_counter = Counter(skills)
+        employment_counter = Counter(employment)
+        area_counter = Counter(area)
+        # print(skills)
+        for name, count in skills_counter.most_common(10):  # Добавляем в результат списки с навыками
+            self.result['requirements'].append({
+                'name': name,
+                'count': count,
+                'percent': f'{round(count / len(skills) * 100, 1)}%'
+            })
 
-skills_counter = Counter(skills)
-employment_counter = Counter(employment)
-area_counter = Counter(area)
-# print(skills)
-for name, count in skills_counter.most_common(10):  # Добавляем в результат списки с навыками
-    result[0]['requirements'].append({
-        'name': name,
-        'count': count,
-        'percent': f'{round(count / len(skills) * 100, 1)}%'
-    })
+        for name, count in employment_counter.most_common():  # Добавляем виды занятости
+            self.result['employment'].append({
+                'name': name,
+                'count': count,
+                'percent': f'{round(count / len(employment) * 100, 1)}%',
+            })
 
-for name, count in employment_counter.most_common():    # Добавляем виды занятости
-    result[0]['employment'].append({
-        'name': name,
-        'count': count,
-        'percent': f'{round(count / len(employment) * 100, 1)}%',
-    })
+        for name, count in area_counter.most_common():  # Добавляем количество вакансий по городам
+            self.result['area'].append({
+                'name': name,
+                'count': count,
+                'percent': f'{round(count / len(area) * 100, 1)}%'
+            })
+        if self.result['employment']:
+            self.result['employment'][0].update({'url_internship': url_alternate})
 
-for name, count in area_counter.most_common():  # Добавляем количество вакансий по городам
-    result[0]['area'].append({
-        'name': name,
-        'count': count,
-        'percent': f'{round(count / len(area) * 100, 1)}%'
-    })
+        pprint.pprint(self.result)
+        with open('hh vacancies.json', 'w', encoding='utf-8') as f:
+            json.dump(self.result, f)
 
-result[0]['employment'][0].update({'url_internship': url_alternate})
+        with open('url_internship.json', 'w+') as f:
+            json.dump(url_alternate, f)
 
+        return self.result
 
-pprint.pprint(result)
-with open('hh vacancies.json', 'w', encoding='utf-8') as f:
-    json.dump(result, f)
-
-with open('url_internship.json', 'w+') as f:
-    json.dump(url_alternate, f)
+    def making_csv(self, name):
+        self.name_csv = f'vacancies {name}.csv'
+        with open(self.name_csv, 'w', encoding='utf-8') as f:
+            file = DictWriter(f, fieldnames=['keywords', 'total_vacancies', 'requirements', 'salary', 'area',
+                                             'employment'],
+                              delimiter=';')
+            file.writeheader()
+            file.writerow(self.result)
+        return self.name_csv
 
 
 if __name__ == '__main__':
-    f = True
-
+    parser = Parser()
+    parser.start_parser(n)
---
+++ hh_telebot.py
@@ -0,0 +1,100 @@
+import telebot
+from telebot import types
+from hh_parser import Parser
+import os
+from os.path import exists
+import time
+import json
+import pprint
+
+TOKEN = '6163221545:AAEgBozoMlE2buBFycJ5ARxa5C5N7VLLHos'
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    user_id = message.chat.id
+    bot.reply_to(message, f'Приветствую Вас {message.chat.first_name}!\n'
+                          f'Добро пожаловать  в чат бот для парсинга сайта hh.ru')
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    itembutparser = types.KeyboardButton('/parser')
+    itembutfile = types.KeyboardButton('/file')
+    itembuthelp = types.KeyboardButton('/help')
+
+    markup.row(itembutparser, itembutfile, itembuthelp)
+
+    bot.send_message(user_id, "Выберите пункт меню: ", reply_markup=markup)
+
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.reply_to((message), f'Данный бот позволяет сформировать отчёт по названию вакансии с сайта hh.ru\n'
+                            f'Отчёт представляет собой файл формата .CSV\n'
+                            f'В нём будут собраны:\n'
+                            f'- Наиболее востребованные навыки\n'
+                            f'- Средняя заработная плата\n'
+                            f'- Количество вакансий в разных городах\n'
+                            f'- Виды занятости, а также ссылки на вакансии со стажировкой.\n'
+                            f'Для начала поиска введите команду /parser, а затем название вакансии\n'
+                            f'После завершения поиска файл с отчётом будет доступен по команде /file')
+
+
+parser = Parser()
+
+
+@bot.message_handler(commands=['parser'])  # Команда просит ввести данные для поиска и запускает парсер
+def run_parser(message):
+    bot.reply_to(message, 'Введите название вакансии для поиска...')
+
+    @bot.message_handler(content_types=['text'])
+    def answer(message):
+        user_id = message.chat.id
+        bot.send_message(user_id, f'Начинаем процесс поиска\n'
+                                  f'Пожалуйста подождите...')
+
+        name_vacancies = message.text
+        parser.start_parser(name_vacancies)  # Передаём название вакансии в парсер
+        parser.making_csv(name_vacancies)  # Создаём файл CSV с результатами
+        bot.send_message(user_id, f'Поиск завершён...\n'
+                                  f'Результаты доступны по команде /file')
+
+
+@bot.message_handler(commands=['file'])  # Команда отправляет файл с результатами парсинга
+def send_file(message):
+    user_id = message.chat.id
+    print(user_id)
+    try:
+        if exists(parser.name_csv):
+            with open(parser.name_csv, encoding='utf-8') as f:
+                print(f)
+                bot.send_document(user_id, f)
+
+    except:
+        bot.send_message(user_id, 'Файл не найден, используйте команду /parser для создания.')
+    else:
+        bot.send_message(user_id, 'Файл не найден, используйте команду /parser для создания.')
+
+
+bot.infinity_polling()
+
+# @bot.message_handler(commands=['parser'])  # Команда просит ввести данные для поиска и запускает парсер
+# def run_parser(message):
+#     bot.reply_to(message, 'Введите название вакансии для поиска...')
+#
+#     @bot.message_handler(content_types=['text'])
+#     def answer(message):
+#         user_id = message.chat.id
+#         name_vacancies = message.text
+#         result = start_parser(name_vacancies)  # Передаём название вакансии в парсер
+#         text = output_print()
+#         sent_message = bot.send_message(user_id, text)
+#         while True:
+#             new_text = output_print()
+#             print(new_text)
+#             if new_text != text:
+#                 text = new_text
+#                 bot.edit_message_text(chat_id=user_id, message_id=sent_message.message_id, text=text)
+#             time.sleep(0.1)
+#             # bot.send_message(user_id, f'Поиск завершён...\n'
+#             #                           f'Результаты доступны по команде /file')
